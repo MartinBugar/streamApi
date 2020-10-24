8cc7a50969b7ee65208afbc04f27ab4759a52e6c
Martin <martin1.bugar@gmail.com>
2020-10-24 13:07:57 +0200

    Optional

---
+++ src/com/prometheus/OptionalExample.java
@@ -0,0 +1,47 @@
+package com.prometheus;
+
+import java.util.Optional;
+
+public class OptionalExample {
+    public static void main(String[] args) {
+
+ //       basicOptional();
+
+        moreOptional();
+    }
+
+    private static void moreOptional() {
+        Optional <String> meno = Optional.of("Martin");
+        Optional <String> prazdne = Optional.empty();
+
+        //metoda vrati false ak je meno prazdne a true ak nie je prazdne
+//        meno.isPresent();
+
+        //vykona akciu vo vnutri tela ak optional nie je prazdny inak vrati false
+//        meno.ifPresent();
+
+        if (meno.isPresent()) {
+            System.out.println("meno : " + meno.get());
+        }else {
+            System.out.println("meno tam nie je");
+        }
+
+        meno.ifPresent(m -> System.out.println("meno je " + m)) ;
+    }
+
+    private static void basicOptional() {
+        Optional<String> meno = Optional.of("Martin");
+        String hodnota1 = "daco";
+        String hodnota2 = null;
+
+        System.out.println("neprazdnom Optional : " + meno);
+        System.out.println("neprazdnom Optional : meno: " + meno.get()); //get vracia hodnotu ktoru drzi premenna meno
+        System.out.println("prazdny Optional: " + Optional.empty()); // vytvori prazdny optional
+
+        System.out.println("ofNullable na neprazdnom Optional: " + Optional.ofNullable(hodnota1));
+        System.out.println("ofNullable na prazdnom Optional: " + Optional.ofNullable(hodnota2));
+
+        //java.lang.NullPointerException
+        System.out.println("ofNullable on Non-Empty Optional : " + Optional.of(hodnota2));
+    }
+}
